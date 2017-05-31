ca9ce9b5dc3f57b5acc2f5d12958ff2e69d3929e
Dean Sheather <dean@deansheather.com>
2017-05-31 16:09:27 +1000

    make CacheConnector easier to extend
    - Add cacheConnector.__parseEvent() (private) for parsing events into
      actions to perform on the underlying cache storage mechanism
    - Make cacheConnector.__process() (private) a method that doesn't need
      to be changed by classes extending CacheConnector
    - Add __get(), __write(), __delete() private methods for classes
      extending CacheConnector to add (so extending CacheConnector is much
      easier)

--- src/lib/CacheConnector.js
+++ src/lib/CacheConnector.js
@@ -5,13 +5,26 @@ const NotImplementedError = require('./errors/NotImplementedError.js');
  */
 const defaultCacheLevel = {
   channels: true,
-  emotes: false,
   guilds: true,
   members: true,
   messages: false,
-  persona: false,
-  roles: true
+  presences: false,
+  roles: true,
+  users: true
 };
+
+/**
+ * Event emitter to use for the master server events.
+ * @type EventEmitter
+ */
+let EventEmitter;
+
+// Load eventemitter3 or Node.js EventEmitter if it's not available
+try {
+  EventEmitter = require('eventemitter3');
+} catch (err) {
+  EventEmitter = require('events').EventEmitter;
+}
 
 /**
  * Cache interface for the framework, attached to a worker by using `attach()`.
@@ -19,7 +32,7 @@ const defaultCacheLevel = {
  * library like Eris caches data for the lifetime of the process (and keeps it
  * up to date with new data).
  */
-class CacheConnector {
+class CacheConnector extends EventEmitter {
   /**
    * Create a new CacheConnector. A CacheConnector stores and retrieves data
    * from a datastore, like a database or an in-memory database. Data is stored
@@ -31,6 +44,7 @@ class CacheConnector {
    * documentation about the `cacheLevel` can be found in the README.
    */
   constructor (cacheLevel) {
+    super();
     if (typeof cacheLevel === 'object') {
       this._cacheLevel = cacheLevel;
     } else {
@@ -59,12 +73,174 @@ class CacheConnector {
   }
 
   /**
+   * Internal method for parsing incoming events into basic commands for the
+   * cache connector.
+   *
+   * The return value of this function is an Object[] with `action`, `key`,
+   * `type`  and `value` keys. `action` is an enumerable (0 = create or replace,
+   * 1 = delete), `key` is a suitable primary key for databases (object ID),
+   * `type` is a string with the object type (channel, emote, guild, member,
+   * message, presence, role) and `value` is an object with the data to store.
+   * If `action == 2`, the `value` key will not be present in the returned
+   * object.
+   * @private
+   * @param {Object} e
+   * @return {Promise<Object[]|null>}
+   */
+  async __parseEvent (e) {
+    switch (e.type) {
+      // TODO: READY
+      case 'CHANNEL_CREATE':
+      case 'CHANNEL_UPDATE':
+        return [{ action: 0, key: e.data.id, type: 'channel', value: e.data }];
+      case 'CHANNEL_DELETE':
+        return [{ action: 1, key: e.data.id, type: 'channel' }];
+      case 'GUILD_CREATE':
+      case 'GUILD_UPDATE':
+        return [{ action: 0, key: e.data.id, type: 'guild', value: e.data }];
+      case 'GUILD_DELETE':
+        return [{ action: 1, key: e.data.id, type: 'guild' }];
+      case 'GUILD_MEMBER_ADD':
+        return [{
+          action: 0,
+          key: e.data.guild_id,
+          type: 'member',
+          value: e.data
+        }];
+      case 'GUILD_MEMBER_REMOVE':
+        return [{ action: 1, key: e.data.user.id, type: 'member' }];
+      case 'GUILD_MEMBER_UPDATE':
+        const key = `${e.data.guild_id}_${e.data.user.id}`;
+        let member = null;
+        try {
+          member = await this.__get('member', key);
+        } catch (e) {}
+
+        // Construct fake old member object to replace missing keys
+        if (!member) {
+          member = {
+            joined_at: '1970-01-01T00:00:00.000000+00:00',
+            deaf: false,
+            mute: false
+          };
+        }
+
+        return [{
+          action: 0,
+          key: `${e.data.guild_id}_${e.data.user.id}`,
+          type: 'member',
+          value: {
+            guild_id: e.data.guild_id,
+            user: e.data.user,
+            roles: e.data.roles,
+            nick: e.data.nick,
+            joined_at: member.joined_at,
+            deaf: member.deaf,
+            mute: member.mute
+          }
+        }];
+      case 'GUILD_MEMBERS_CHUNK':
+        return e.data.members.map(m => {
+          return {
+            action: 0,
+            key: `${e.data.guild_id}_${m.id}`,
+            type: 'member',
+            value: m
+          };
+        });
+      case 'GUILD_ROLE_CREATE':
+      case 'GUILD_ROLE_UPDATE':
+        e.data.role.guild_id = e.data.guild_id;
+        return [{
+          action: 0,
+          key: e.data.role.id,
+          type: 'role',
+          value: e.data
+        }];
+      case 'GUILD_ROLE_DELETE':
+        return [{ action: 1, key: e.data.role_id, type: 'role' }];
+      // TODO: MESSAGE_*
+      // TODO: MESSAGE_DELETE_BULK
+      case 'PRESENCE_UPDATE':
+        e.data.user = { id: e.data.user.id };
+        return [{
+          action: 0,
+          key: `${e.data.guild_id}_${e.data.user.id}`,
+          type: 'presence',
+          value: e.data
+        }];
+      case 'USER_UPDATE':
+        return [{ action: 0, key: e.data.id, type: 'user', value: e.data }];
+      // TODO: VOICE_STATE_UPDATE
+      // case 'VOICE_STATE_UPDATE':
+      //   return {
+      //     action: 0,
+      //     key: e.data.?,
+      //     type: 'voicestate',
+      //     value: e.data
+      //   };
+    }
+    return [];
+  }
+
+  /**
    * Internal method for processing incoming events.
    * @param {Object} event
    */
-  __process (event) {
+  async __process (event) {
+    const actions = await this.__parseEvent(event);
+
+    // Commit actions to cache
+    // TODO: catch errors and retry failed actions
+    for (let a of actions) {
+      if (!this._cacheLevel[a.type + 's']) continue;
+      switch (a.action) {
+        case 0:
+          await this.__write(a.type, a.key, a.value);
+          break;
+        case 1:
+          await this.__delete(a.type, a.key);
+          break;
+      }
+    }
+  }
+
+  // TODO: getChannel(id), getGuild(id), getMember(guild_id, user_id), ...
+
+  /**
+   * Get method for retrieving data from the cache.
+   * @param {String} type
+   * @param {String} key
+   * @return {Promise<Object|null>}
+   */
+  async __get (type, key) {
     throw new NotImplementedError(
-      `__process() is not implemented for ${this.constructor.name}`
+      `__get() is not implemented for ${this.constructor.name}`
+    );
+  }
+
+  /**
+   * Get method for writing data to the cache.
+   * @param {String} type
+   * @param {String} key
+   * @param {Object} value
+   * @return {Promise}
+   */
+  async __write (type, key, value) {
+    throw new NotImplementedError(
+      `__write() is not implemented for ${this.constructor.name}`
+    );
+  }
+
+  /**
+   * Get method for deleting data from the cache.
+   * @param {String} type
+   * @param {String} key
+   * @return {Promise}
+   */
+  async __delete (type, key) {
+    throw new NotImplementedError(
+      `__delete() is not implemented for ${this.constructor.name}`
     );
   }
 }
